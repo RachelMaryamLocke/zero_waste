7b06ea813facae0648fce02edebc5b0435b6ed20
sophielou987 <sophie.lou.95@hotmail.co.uk>
2019-06-23 19:45:42 +0100

    adding quiz files

---
+++ zero_waste.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+
+app = Flask("MyApp")
+@app.route("/")
+def hello():
+    return "hello world"
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
+@app.route("/answer", methods=["POST"])
+def answer1():
+    Q1 = request.form['Q1']
+    Q2 = request.form['Q2']
+    Q3 = request.form['Q3']
+    Q4 = request.form['Q4']
+    if Q1 == "Q1A1":
+         Q1 = 1
+    elif Q1 == "Q1A2":
+         Q1 = 2
+    elif Q1 == "Q1A3":
+         Q1 = 3
+    else:
+         Q1 = 4
+    if Q2 == "Q2A1":
+         Q2 = 1
+    elif Q2 == "Q2A2":
+         Q2 = 2
+    elif Q2 == "Q2A3":
+         Q2 = 3
+    else:
+         Q2 = 4
+    if Q3 == "Q3A1":
+         Q3 = 1
+    elif Q3 == "Q3A2":
+         Q3 = 2
+    else:
+         Q3 = 3
+    if Q4 == "Q4A1":
+         Q4 = 1
+    elif Q4 == "Q4A2":
+         Q4 = 2
+    else:
+         Q4 = 3
+
+    score = Q1+Q2+Q3+Q4
+    return render_template("answer.html", Q1=Q1, Q2=Q2, Q3=Q3, Q4=Q4, score=score)
+
+
+
+app.run(debug=True)
