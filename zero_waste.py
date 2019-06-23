e6d07aa28954da05117da76eecf571da260e2832
Rachel Locke <rachellockedigital@gmail.com>
2019-06-23 20:20:20 +0100

    update
    changes made to store locator, homepage and app routes

--- zero_waste.py
+++ zero_waste.py
@@ -1,12 +1,30 @@
 from flask import Flask, render_template, request
+import requests
+import sqlite3 as sql #Import Sqlite
 
 app = Flask("MyApp")
-@app.route("/")
-def hello():
-    return "hello world"
+
+# def send_confirmation_message(booking_email, booking_name, booking_date): #Irinas code for using the mailgun API send a confirmation email
+# 	return requests.post(
+# 		"https://api.mailgun.net/v3/sandbox987b713dd9344dc88c145724a2d28d92.mailgun.org/messages",
+# 		auth=("api","4c7dc4bd1c814f264622f67b1eeb69ce-3b1f59cf-225d56b9"),
+# 		data={"from": "The Scarlet Letter <mailgun@sandbox987b713dd9344dc88c145724a2d28d92.mailgun.org>",
+# 			"to" :[booking_email],
+# 			"subject": "Booking confirmation",
+# 			"text": "Dear {}, this is to confirm your booking for {} at The Scarlet Letter Cocktail bar".format(booking_name, booking_date)})
+
+@app.route("/") #app.route is specific to flask and forms the url paths for your web application
+def index(): #personally called this method index to represent the fact it will return the index page
+    return render_template('sustainable.html') #render_template is a flask specific method that returns the html page you want to display
+                                         #render_template .html files are stored in the templates folder (required for flask to work correctly)
+@app.route("/map") #same functionality as above
+def map():
+    return render_template("map.html")
+
 @app.route("/quiz")
 def quiz():
     return render_template("quiz.html")
+    
 @app.route("/answer", methods=["POST"])
 def answer1():
     Q1 = request.form['Q1']
